5c318aaa51ab56f73d52fa03b3ce06246b520119
skynettw <skynet.tw@gmail.com>
2023-03-20 16:45:08 +0800

    0320 4th update

--- 0320c.py
+++ 0320c.py
@@ -2,6 +2,13 @@ f = open("bodyinfo.csv", "r", encoding="utf-8")
 rawdata = f.readlines()
 f.close()
 
+data = [item.split(",") for item in rawdata]
+heights = [int(d[1]) for d in data]
+weights = [int(d[2]) for d in data]
+print("本班最高的學生是{}公分".format(max(heights)))
+print("本班最矮的學生是{}公分".format(min(heights)))
+print("本班平均身高是{}公分".format(round(sum(heights)/len(heights)),2))
+'''
 names = list()
 heights = list()
 weights = list()
@@ -10,6 +17,7 @@ for item in rawdata:
     names.append(name)
     heights.append(height)
     weights.append(weight)
-print(names)
-print(heights)
-print(weights)
+'''
+#print(names)
+#print(heights)
+#print(weights)
