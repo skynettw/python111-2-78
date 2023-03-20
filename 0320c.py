1ee46e35f3f4ca835765394ca34a6481566ec0db
skynettw <skynet.tw@gmail.com>
2023-03-20 16:46:58 +0800

    0320 4th update

--- 0320c.py
+++ 0320c.py
@@ -5,6 +5,13 @@ f.close()
 data = [item.split(",") for item in rawdata]
 heights = [int(d[1]) for d in data]
 weights = [int(d[2]) for d in data]
+bmis = list()
+for d in data:
+    w = int(d[2])
+    h = int(d[1])
+    bmi = w / (h/100)**2
+    bmis.append(round(bmi, 2))
+print(bmis)
 print("本班最高的學生是{}公分".format(max(heights)))
 print("本班最矮的學生是{}公分".format(min(heights)))
 print("本班平均身高是{}公分".format(round(sum(heights)/len(heights)),2))
