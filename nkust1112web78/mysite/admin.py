4f5bdf0f77341f3df540e746c0847e81c74de3d2
skynettw <skynet.tw@gmail.com>
2023-03-27 16:54:40 +0800

    0327 6th update

--- nkust1112web78/mysite/admin.py
+++ nkust1112web78/mysite/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from mysite import models        # 從 mysite 的資料夾中的 models.py 匯入所有的類別（資料表）
 
-# Register your models here.
+admin.site.register(models.HBicycleData)
+
