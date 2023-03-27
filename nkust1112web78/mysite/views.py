ce90be666b940487aae1e6b653f728f915adfccd
skynettw <skynet.tw@gmail.com>
2023-03-27 16:03:41 +0800

    0327 1st commit

--- nkust1112web78/mysite/views.py
+++ nkust1112web78/mysite/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render   # 渲染網頁
 from django.http import HttpResponse   # Django用來回應給瀏覽器特定資料的函式
 import requests   # 匯入擷取網頁所需要的模組
 import json       # 匯入操作JSON格式所需要的模組
 
 def index(request):
+    myname = "高科金城武"
+    return render(request, "index.html", locals())
+
+def all_data(request):
     url = "https://opendata.hccg.gov.tw/OpenDataFileHit.ashx?ID=48DEDBDAC3A31FC6&u=77DFE16E459DFCE3F5CEA2F931E333F7E23D5729EF83D5F20744125E844FB27044F9892E6F09372518441B3BB84260426ADE242A57DFB9E8C9A50C50134F4F47"
 
     r = requests.get(url)         
