1813b40f97d28f48eabf0b228458c907f83c09bf
skynettw <skynet.tw@gmail.com>
2023-03-27 17:11:39 +0800

    0327 7th update:

--- nkust1112web78/mysite/views.py
+++ nkust1112web78/mysite/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render   # 渲染網頁
 from django.http import HttpResponse   # Django用來回應給瀏覽器特定資料的函式
 import requests   # 匯入擷取網頁所需要的模組
 import json       # 匯入操作JSON格式所需要的模組
+from mysite import models  # 從 mysite 的資料夾中的 models.py 匯入所有的類別（資料表）
 
 def index(request):
     myname = "高雄金城武"
@@ -22,4 +23,21 @@ def all_data(request):
     return HttpResponse(msg)
 
 def filtered_data(request):
+    # 先把舊資料通通刪除
+    models.HBicycleData.objects.all().delete()
+    # 要比照all_data函式的程式，把網站上所有的資料都下載解析，放到資料表 (HBicycleData) 裡面
+    url = "https://opendata.hccg.gov.tw/OpenDataFileHit.ashx?ID=48DEDBDAC3A31FC6&u=77DFE16E459DFCE3F5CEA2F931E333F7E23D5729EF83D5F20744125E844FB27044F9892E6F09372518441B3BB84260426ADE242A57DFB9E8C9A50C50134F4F47"
+    r = requests.get(url)         
+    data = json.loads(r.text)     
+    bicycle_data = data['retVal'] 
+    for item in bicycle_data:
+        new_record = models.HBicycleData(
+            sna = item['sna'].split("_")[1],
+            sbi = int(item['sbi']),
+            tot = int(item['tot'])
+            )
+        new_record.save()
+
+    # 過濾 HBicycleData 裡面的所有記錄，找出其中sbi>=10的站台放到data中
+    data = models.HBicycleData.objects.filter(sbi__gte=10)
     return render(request, "filter.html", locals())
