1189429db24c6cd6a59ed0e3f523c1dd6fe76c12
skynettw <skynet.tw@gmail.com>
2023-04-10 17:05:23 +0800

    0410d

--- nkust1112web78/mysite/views.py
+++ nkust1112web78/mysite/views.py
@@ -10,6 +10,9 @@ def index(request):
     myname = random.choice(mynames)
     return render(request, "index.html", locals())
 
+def nkustnews(request):
+    return render(request, "nkustnews.html", locals())
+
 def all_data(request):
     url = "https://opendata.hccg.gov.tw/OpenDataFileHit.ashx?ID=48DEDBDAC3A31FC6&u=77DFE16E459DFCE3F5CEA2F931E333F7E23D5729EF83D5F20744125E844FB27044F9892E6F09372518441B3BB84260426ADE242A57DFB9E8C9A50C50134F4F47"
 
