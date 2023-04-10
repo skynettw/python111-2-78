ff60032c3c3c5090f455aa6ee1c4aeaa8890fd59
skynettw <skynet.tw@gmail.com>
2023-04-10 17:02:22 +0800

    0410c

--- nkust1112web78/nkust1112web78/urls.py
+++ nkust1112web78/nkust1112web78/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from mysite import views     # 從views.py去匯入所有的處理函式
 
 urlpatterns = [
+    path('news/', views.nkustnews),
     path('all/', views.all_data),         # 顯示所有的站
     path("filter/", views.filtered_data), # 顯示超過10台可用自行車的站
     path('', views.index ),  # 如果有人來瀏覽首頁的話，請交給views.py裡面的index()函式處理
